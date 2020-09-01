448657409cf8052e94c18e10bf26e41149f0882f
Abhishek KP <42582013+AdolfCritlerr@users.noreply.github.com>
2020-09-01 14:48:40 +0100

    Update Qrcode.py

--- Qrcode.py
+++ Qrcode.py
@@ -6,6 +6,6 @@ QRstring = input("website :")  #paste any URL
 url = pyqrcode.create(QRstring)
 
 directory = (input("Path to save the file :")) #path to save the file
-#format for eg:--- D:\filename or C:\filename
-file_name = input("file name: ")
+#format for eg:--- D:\path or C:\path
+file_name = input("file name: ") #name of the file to be written with .png extension
 url.png(directory + file_name, scale = 8)
